990de3f2d0ae650f45b86c91bb729f6ddcf4ec5c
marcespasa11 <marcespasa11@gmail.com>
2024-07-10 17:58:40 +0200

    prova

---
+++ GameBuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GameBuddy
+//
+//  Created by Marc Espasa González on 10/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GameBuddy/GameBuddyApp.swift
@@ -0,0 +1,19 @@
+//
+//  GameBuddyApp.swift
+//  GameBuddy
+//
+//  Created by Marc Espasa González on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct GameBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+            
+        }
+    }
+}
