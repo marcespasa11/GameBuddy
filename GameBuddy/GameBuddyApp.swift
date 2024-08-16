28ff2147661ffdd0b3b12ee255328d47764e9217
marcespasa11 <marcespasa11@gmail.com>
2024-08-16 11:39:14 +0200

    API Google maps

--- GameBuddy/GameBuddyApp.swift
+++ GameBuddy/GameBuddyApp.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 import FirebaseCore
 import FirebaseAuth
+import GoogleMaps
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
     //Auth.auth().useEmulator(withHost: "localhost", port: 9099)
-    
+      if let apiKey = Bundle.main.object(forInfoDictionaryKey: "GOOGLE_MAPS_API_KEY") as? String {
+                  GMSServices.provideAPIKey(apiKey)
+              }
     return true
   }
 }
