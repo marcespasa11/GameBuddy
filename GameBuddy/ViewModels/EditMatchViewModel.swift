df40bc80c3a2f2d131f4f2bf42f89815f3c1dc1f
marcespasa11 <marcespasa11@gmail.com>
2024-08-21 19:25:25 +0200

    Map implemented in EditMatch

--- GameBuddy/ViewModels/EditMatchViewModel.swift
+++ GameBuddy/ViewModels/EditMatchViewModel.swift
@@ -9,6 +9,7 @@ import Foundation
 import FirebaseFirestore
 import FirebaseAuth
 import SwiftUI
+import CoreLocation
 
 class EditMatchViewModel: ObservableObject {
     @Published var selectedMatchType: MatchType
@@ -17,6 +18,8 @@ class EditMatchViewModel: ObservableObject {
     @Published var matchDescription: String
     @Published var showAlert: Bool = false
     @Published var alertMessage: String = ""
+    @Published var matchLocation: IdentifiableLocation?
+    @Published var address: String = "Loading address..."  // Propiedad para manejar la dirección
     
     private var db = Firestore.firestore()
     @Published var userSession: UserSession
@@ -30,19 +33,30 @@ class EditMatchViewModel: ObservableObject {
         self.matchDate = match.date
         self.maxPlayers = match.maxPlayers
         self.matchDescription = match.description
+        self.matchLocation = IdentifiableLocation(coordinate: CLLocationCoordinate2D(latitude: match.location.latitude, longitude: match.location.longitude))
+        self.address = match.address ?? "Address not available"  // Cargar la dirección existente
     }
     
     var isFormValid: Bool {
         !matchDescription.isEmpty
     }
-    
+
     func updateMatch() {
         match.type = selectedMatchType.rawValue
         match.date = matchDate
         match.maxPlayers = maxPlayers
         match.description = matchDescription
         
-        updateMatchInFirestore()
+        if let matchLocation = matchLocation {
+            match.location = Location(latitude: matchLocation.coordinate.latitude, longitude: matchLocation.coordinate.longitude)
+            reverseGeocodeLocation(for: matchLocation.coordinate) { [weak self] updatedAddress in
+                self?.match.address = updatedAddress
+                self?.address = updatedAddress
+                self?.updateMatchInFirestore()  // Solo actualizar Firestore después de que la dirección se haya actualizado
+            }
+        } else {
+            updateMatchInFirestore()  // Si la ubicación no cambió, simplemente actualiza el partido
+        }
     }
     
     private func updateMatchInFirestore() {
@@ -70,6 +84,29 @@ class EditMatchViewModel: ObservableObject {
             print("Error encoding match: \(error)")
             self.alertMessage = "Failed to encode match data"
             self.showAlert = true
+        }
+    }
+
+    func reverseGeocodeLocation(for coordinate: CLLocationCoordinate2D, completion: @escaping (String) -> Void) {
+        let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        let geocoder = CLGeocoder()
+        
+        geocoder.reverseGeocodeLocation(location) { placemarks, error in
+            if let error = error {
+                print("Error during reverse geocoding: \(error.localizedDescription)")
+                completion("Address not available")
+                return
+            }
+            
+            let placemark = placemarks?.first
+            let addressString = [
+                placemark?.thoroughfare, // Calle
+                placemark?.subThoroughfare, // Número
+                placemark?.locality // Ciudad
+            ].compactMap { $0 }.joined(separator: ", ")
+            
+            let finalAddress = addressString.isEmpty ? "Address not available" : addressString
+            completion(finalAddress)
         }
     }
 }
--- GameBuddy/Views/EditMatchView.swift
+++ GameBuddy/Views/EditMatchView.swift
@@ -6,49 +6,129 @@
 //
 
 import SwiftUI
+import MapKit
+import CoreLocation
 
 struct EditMatchView: View {
     @Environment(\.presentationMode) var presentationMode
     @EnvironmentObject var userSession: UserSession
     @StateObject private var viewModel: EditMatchViewModel
+    @State private var region: MKCoordinateRegion
+    @State private var selectedLocation: CLLocationCoordinate2D?
 
     init(match: Match) {
         _viewModel = StateObject(wrappedValue: EditMatchViewModel(match: match, userSession: UserSession()))
+        _region = State(initialValue: MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: match.location.latitude, longitude: match.location.longitude),
+            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        ))
+        _selectedLocation = State(initialValue: CLLocationCoordinate2D(latitude: match.location.latitude, longitude: match.location.longitude))
     }
 
     var body: some View {
-        Form {
-            Section(header: Text("Match Type")) {
-                Picker("Select Match Type", selection: $viewModel.selectedMatchType) {
-                    ForEach(MatchType.allCases, id: \.self) { type in
-                        Text(type.rawValue).tag(type)
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 20) {
+                    // Mapa en la parte superior con marcador en la ubicación del partido
+                    ZStack {
+                        MapViewRepresentable(region: $region, selectedLocation: $selectedLocation)
+                            .frame(height: 300)
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                        
+                        VStack {
+                            Spacer()
+                            HStack {
+                                Spacer()
+                                VStack {
+                                    Button(action: {
+                                        zoomIn()
+                                    }) {
+                                        Image(systemName: "plus.magnifyingglass")
+                                            .padding(10)
+                                            .background(Color.white)
+                                            .clipShape(Circle())
+                                            .shadow(radius: 5)
+                                    }
+                                    .padding(.bottom, 8)
+                                    
+                                    Button(action: {
+                                        zoomOut()
+                                    }) {
+                                        Image(systemName: "minus.magnifyingglass")
+                                            .padding(10)
+                                            .background(Color.white)
+                                            .clipShape(Circle())
+                                            .shadow(radius: 5)
+                                    }
+                                }
+                                .padding()
+                            }
+                        }
                     }
+
+                    VStack(alignment: .leading, spacing: 16) {
+                        // Tipo de Partido
+                        Text("Match Type")
+                            .font(.headline)
+                        Picker("Select Match Type", selection: $viewModel.selectedMatchType) {
+                            ForEach(MatchType.allCases, id: \.self) { type in
+                                Text(type.rawValue).tag(type)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+
+                        // Detalles del Partido
+                        Text("Details")
+                            .font(.headline)
+                        VStack(spacing: 16) {
+                            DatePicker("Select Date and Time", selection: $viewModel.matchDate, displayedComponents: [.date, .hourAndMinute])
+                                .datePickerStyle(GraphicalDatePickerStyle())
+                                .labelsHidden()
+
+                            Stepper("Max Players: \(viewModel.maxPlayers)", value: $viewModel.maxPlayers, in: 2...20)
+
+                            TextField("Description", text: $viewModel.matchDescription)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                        }
+                    }
+                    .padding(.horizontal)
+
+                    // Botón de Guardar Cambios
+                    Button(action: {
+                        viewModel.presentationMode = presentationMode
+                        if let selectedLocation = selectedLocation {
+                            viewModel.matchLocation = IdentifiableLocation(coordinate: selectedLocation)
+                        }
+                        viewModel.updateMatch()
+                    }) {
+                        Text("Save Changes")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(viewModel.isFormValid ? Color.blue : Color.gray)
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                    }
+                    .disabled(!viewModel.isFormValid)
                 }
-                .pickerStyle(SegmentedPickerStyle())
+                .padding(.top)
             }
-
-            Section(header: Text("Details")) {
-                DatePicker("Select Date and Time", selection: $viewModel.matchDate, displayedComponents: [.date, .hourAndMinute])
-
-                Stepper("Max Players: \(viewModel.maxPlayers)", value: $viewModel.maxPlayers, in: 2...20)
-
-                TextField("Description", text: $viewModel.matchDescription)
-            }
-            
-            Section {
-                Button(action: {
-                    viewModel.presentationMode = presentationMode
-                    viewModel.updateMatch()
-                }) {
-                    Text("Save Changes")
-                        .frame(maxWidth: .infinity, alignment: .center)
-                }
-                .disabled(!viewModel.isFormValid)
+            .navigationTitle("Edit Match")
+            .alert(isPresented: $viewModel.showAlert) {
+                Alert(title: Text("Match Update"), message: Text(viewModel.alertMessage), dismissButton: .default(Text("OK")))
             }
         }
-        .navigationTitle("Edit Match")
-        .alert(isPresented: $viewModel.showAlert) {
-            Alert(title: Text("Match Update"), message: Text(viewModel.alertMessage), dismissButton: .default(Text("OK")))
-        }
+    }
+
+    private func zoomIn() {
+        region.span.latitudeDelta /= 2.0
+        region.span.longitudeDelta /= 2.0
+    }
+
+    private func zoomOut() {
+        region.span.latitudeDelta *= 2.0
+        region.span.longitudeDelta *= 2.0
     }
 }
